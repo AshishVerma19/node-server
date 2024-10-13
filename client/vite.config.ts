fb3b3c3428ddbf1e4115b010e290a91794de1dd0
Ashish Verma <vermaashish1201@hotmail.com>
2024-10-13 18:01:48 +0530

    enable server to render client dist folder

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "../server/src/client/dist", // Ensures the build is stored in the root dist folder
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000", // Proxy API requests to the backend server in development
+    },
+  },
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { print } from "@services/print";
+import path from "path";
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.PORT ?? 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, TypeScript + Node.js + Express!");
+app.get("/api/user", (req, res) => {
+  res.send({ name: "ashish vers" });
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+// All other GET requests not handled will return the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
 });
 
 app.listen(port, () => {
